b9e2443cbba2611da7d2f5b928eb2c51fdcf8190
FelixHommel <felixhommel04@gmail.com>
2026-01-30 23:03:52 +0100

    ci: change code coverage system to python
    Use one python script to configure code coverage instead of various bash
    scripts for improved usability.

--- scripts/coverage.py
+++ scripts/coverage.py
@@ -9,27 +9,71 @@ from pathlib import Path
 
 
 def run_command(cmd: list[str]) -> None:
+    """Execute a command on the system.
+
+    Parameters
+    ----------
+    cmd : list[str]
+        The command that is going to be executed.
+    """
+
     print("+", " ".join(shlex.quote(c) for c in cmd))
     subprocess.run(cmd, check=True)
 
 
 def main() -> int:
+    """Execute the main coverage analysis logic.
+
+    Set up the script arguments and process the coverage data to produce refined results.
+
+    Returns
+    -------
+    int
+        0 if successful, anything else otherwise
+    """
+
     parser = argparse.ArgumentParser(
-        description="Generate LCOV and optional HTML coverage reports."
+        description="Generate LCOV code coverage report and optionally generate a HTML version of it."
     )
     parser.add_argument(
-        "--build-dir", type=Path, default="build/code-coverage", help=""
+        "--build-dir",
+        type=Path,
+        default="./build/code-coverage",
+        help="Build directory containing gcda/gcno files.",
     )
     parser.add_argument(
-        "--out-dir", type=Path, default="build/code-coverage/coverage-data", help=""
+        "--out-dir",
+        type=Path,
+        default="./build/code-coverage/coverage-data",
+        help="Output directory for report data.",
     )
-    parser.add_argument("--raw", default="coverage.raw.info", help="")
-    parser.add_argument("--filtered", default="coverage.filtered.info", help="")
-    parser.add_argument("--html", action="store_true", help="")
-    parser.add_argument("--html-dir", type=Path, default="coverage-report", help="")
-    parser.add_argument("--zerocounters", action="store_true", help="")
+    parser.add_argument(
+        "--raw",
+        type=str,
+        default="coverage.raw.info",
+        help="Raw LCOV filename (including everything).",
+    )
+    parser.add_argument(
+        "--filtered",
+        type=str,
+        default="coverage.filtered.info",
+        help="Filtered LCOV filename.",
+    )
+    parser.add_argument("--html", action="store_true", help="Generate HTML report.")
+    parser.add_argument(
+        "--html-dir",
+        type=Path,
+        default="coverage-report",
+        help="HTML report output directory.",
+    )
+    parser.add_argument(
+        "--zerocounters",
+        action="store_true",
+        help="Reset counters before running tests/capture.",
+    )
     parser.add_argument(
         "--exclude",
+        type=list[str],
         action="append",
         default=[
             "/usr/*",
@@ -42,37 +86,50 @@ def main() -> int:
     )
     args = parser.parse_args()
 
-    args.out_dir.mkdir(parents=True, exist_ok=True)
+    out = Path(args.out_dir)
+    out.mkdir(parents=True, exist_ok=True)
 
     if args.zerocounters:
         run_command(["lcov", "--directory", str(args.build_dir), "--zerocounters"])
 
-    raw = args.out_dir / args.raw
-    run_command([
-        "lcov",
-        "--directory", str(args.build_dir),
-        "--capture",
-        "--ignore-errors", "mismatch",
-        "--output-file", str(raw)
-    ])
-    filtered = args.out_dir / args.filtered
-    run_command([
-        "lcov",
-        "--ignore-errors", "unused"
-        "--remove", str(raw), *args.exclude,
-        "--output-file", str(filtered)
-    ])
+    # NOTE: Step 1) Gather all data
+    raw = out / args.raw
+    run_command(
+        [
+            "lcov",
+            "--directory",
+            str(args.build_dir),
+            "--capture",
+            "--ignore-errors",
+            "mismatch",
+            "--output-file",
+            str(raw),
+        ]
+    )
+
+    # NOTE: Step 2) Filter out unrelevant data
+    filtered = out / args.filtered
+    run_command(
+        [
+            "lcov",
+            "--ignore-errors",
+            "unused",
+            "--remove",
+            str(raw),
+            *args.exclude,
+            "--output-file",
+            str(filtered),
+        ]
+    )
 
     if args.html:
-        html_dir = args.out_dir / args.html_dir
+        html_dir = out / args.html_dir
         html_dir.mkdir(parents=True, exist_ok=True)
-        run_command([
-            "genhtml", str(filtered),
-            "--output-directory", str(html_dir)
-        ])
-        print(f"HTML report: {html_dir}/index.html")
+        run_command(["genhtml", str(filtered), "--output-directory", str(html_dir)])
+        print(f"HTML report: {html_dir.absolute()}/index.html")
 
-    print(f"LCOV report: {filtered}")
+    print(f"LCOV report: {filtered.absolute()}")
+
     return 0
 
 
