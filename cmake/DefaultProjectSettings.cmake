b52975766e0da797a7c947212acf233f038b8e49
FelixHommel <felixhommel04@gmail.com>
2026-01-31 23:09:07 +0100

    build: separate debug from assertion definition
    Split the intention from the SFA_DEBUG macro, now there is SFA_DEBUG for
    extended debug feautres (logging, ...) and SFA_ENABLE_ASSERTIONS for
    usage of assertions.

--- cmake/DefaultProjectSettings.cmake
+++ cmake/DefaultProjectSettings.cmake
@@ -17,6 +17,12 @@ endif()
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 option(SFA_DEBUG "Enable debug statements and asserts" OFF)
-if(SFA_DEBUG OR CMAKE_BUILD_TYPE STREQUAL "Debug")
-    add_compile_definitions(SFA_DEBUG)
+option(SFA_ENABLE_ASSERTIONS "Enable assertions" OFF)
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    set(SFA_DEBUG ON CACHE BOOL "" FORCE)
+    set(SFA_ENABLE_ASSERTIONS ON CACHE BOOL "" FORCE)
 endif()
+
+add_compile_definitions(SFA_DEBUG=$<BOOL:${SFA_DEBUG}>)
+add_compile_definitions(SFA_ENABLE_ASSERTIONS=$<BOOL:${SFA_ENABLE_ASSERTIONS}>)
+
