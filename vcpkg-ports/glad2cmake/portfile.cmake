b5b54c15f955c7221edf6dcefa3275078dcc5018
FelixHommel <felixhommel04@gmail.com>
2026-02-04 18:25:13 +0100

    fix: make glad2cmake a statically linked library
    This should fix the link issues in the Windows CI jobs

--- vcpkg-ports/glad2cmake/portfile.cmake
+++ vcpkg-ports/glad2cmake/portfile.cmake
@@ -5,6 +5,8 @@ vcpkg_from_github(
     SHA512 197482d85da5a5b93cd5e3ea27600fefad86a5eb63359c6fbe12ea67d7fb9b39c3a44c79543478718e6505cf3a16f41dae530cb971470c674cfa4e0c35daeb15
 )
 
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
