3ac1b3a973f8544c7b724f5689daa562db1ab06e
FelixHommel <felixhommel04@gmail.com>
2026-02-25 11:47:05 +0100

    feat: Introduce custom exception types

---
+++ src/engine/utility/exceptions/Exception.hpp
@@ -0,0 +1,47 @@
+#ifndef SFA_SRC_ENGINE_UTILITY_EXCEPTIONS_EXCEPTION_HPP
+#define SFA_SRC_ENGINE_UTILITY_EXCEPTIONS_EXCEPTION_HPP
+
+#include <fmt/format.h>
+
+#include <exception>
+#include <source_location>
+#include <string>
+#include <utility>
+
+namespace sfa
+{
+
+class Exception : public std::exception
+{
+public:
+    explicit Exception(std::string message, std::source_location location = std::source_location::current())
+        : m_message(std::move(message))
+        , m_location(std::move(location))
+    {
+        Exception::buildWhatMessage();
+    }
+
+    [[nodiscard]] const char* what() const noexcept override { return m_whatMessage.c_str(); }
+    [[nodiscard]] const std::source_location& location() const noexcept { return m_location; }
+
+protected:
+    std::string m_message;
+    std::source_location m_location;
+    std::string m_whatMessage;
+
+    virtual void buildWhatMessage()
+    {
+        m_whatMessage = fmt::format(
+            "Exception: {}\n\tat {}: {} in {}",
+            m_message,
+            m_location.file_name(),
+            m_location.line(),
+            m_location.function_name()
+        );
+    }
+};
+
+} // namespace sfa
+
+#endif // !SFA_SRC_ENGINE_UTILITY_EXCEPTIONS_EXCEPTION_HPP
+
---
+++ src/test/utility/exceptions/ExceptionTest.cpp
@@ -0,0 +1,38 @@
+#include "utility/exceptions/Exception.hpp"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <source_location>
+
+namespace
+{
+
+constexpr auto TEST_MESSAGE{ "TestMessage" };
+constexpr auto throwingStatement = [] { throw sfa::Exception{ TEST_MESSAGE }; };
+
+} // namespace
+
+namespace sfa::testing
+{
+
+/// \brief Test the most basic way to create an exception.
+///
+/// In this case the source location should be the exact line where, the exception was thrown, in the same function.
+TEST(ExceptionTest, DefaultConstruction)
+{
+    Exception e{ ::TEST_MESSAGE };
+
+    EXPECT_STREQ(e.location().function_name(), std::source_location::current().function_name());
+}
+
+/// \brief Test that the exception contains the user message when it is thrown.
+///
+/// When the exception is thrown, the message should contain the message the user specified
+TEST(ExceptionTest, ExceptionMessgae)
+{
+    EXPECT_THAT(::throwingStatement, ::testing::ThrowsMessage<Exception>(::testing::HasSubstr(::TEST_MESSAGE)));
+}
+
+} // namespace sf::testing
+
