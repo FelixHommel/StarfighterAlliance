d6a6c751cc59902ebc7178c7d8b67443f72e7e23
FelixHommel <felixhommel04@gmail.com>
2026-03-01 16:13:43 +0100

    feat: add system to apply transform to UI elements

--- src/engine/ecs/systems/UITransformSystem.cpp
+++ src/engine/ecs/systems/UITransformSystem.cpp
@@ -1,6 +1,8 @@
 #include "UITransformSystem.hpp"
 
+#include "ecs/ComponentArray.hpp"
 #include "ecs/ComponentRegistry.hpp"
+#include "ecs/ECSUtility.hpp"
 #include "ecs/components/UIHierarchyComponent.hpp"
 #include "ecs/components/UITransformComponent.hpp"
 
@@ -17,16 +19,29 @@ void UITransformSystem::update(ComponentRegistry& registry)
     for(std::size_t i{ 0 }; i < hierarchies.size(); ++i)
     {
         const auto entity{ hierarchies.entityAtIndex(i) };
-        const auto& hierarchy{ hierarchies.get(entity) };
-        const auto& parentTransform{ transforms.get(entity) };
 
-        for(const auto& child : hierarchy.children)
-        {
-            auto& childTransform{ transforms.get(child) };
-
-            childTransform.worldPosition = parentTransform.worldPosition + childTransform.localPosition;
-        }
+        if(hierarchies.get(entity).parent == NULL_ENTITY)
+            propagate(entity, transforms, hierarchies);
     }
+}
+
+/// \brief Recursively apply transforms to elements and their child elements.
+///
+/// \param entity the current entity that is processed
+/// \param transforms a \ref ComponentArray with all \ref UITransformComponent stored in it
+/// \param hierarchies q \ref ComponentArray with all \ref UIHierarchyComponent stored in it
+void UITransformSystem::propagate(EntityID entity, ComponentArray<UITransformComponent>& transforms, const ComponentArray<UIHierarchyComponent>& hierarchies)
+{
+    const auto& hierarchy{ hierarchies.get(entity) };
+    auto& transform{ transforms.get(entity) };
+
+    if(hierarchy.parent != NULL_ENTITY)
+        transform.worldPosition = transforms.get(hierarchy.parent).worldPosition + transform.localPosition;
+    else
+        transform.worldPosition = transform.localPosition;
+
+    for(const auto& child : hierarchy.children)
+        propagate(child, transforms, hierarchies);
 }
 
 } // namespace sfa
--- src/engine/ecs/systems/UITransformSystem.hpp
+++ src/engine/ecs/systems/UITransformSystem.hpp
@@ -2,6 +2,8 @@
 #define SFA_SRC_ENGINE_ECS_SYSTEMS_UI_TRANSFORM_SYSTEM_HPP
 
 #include "ecs/ComponentRegistry.hpp"
+#include "ecs/components/UIHierarchyComponent.hpp"
+#include "ecs/components/UITransformComponent.hpp"
 
 namespace sfa
 {
@@ -13,7 +15,13 @@ namespace sfa
 class UITransformSystem
 {
 public:
+    /// \brief Update the positions of UI components.
+    /// 
+    /// \param registry the \ref ComponentRegistry to access the UI elements
     static void update(ComponentRegistry& registry);
+
+private:
+    static void propagate(EntityID entity, ComponentArray<UITransformComponent>& transforms, const ComponentArray<UIHierarchyComponent>& hierarchies);
 };
 
 } // namespace sfa
