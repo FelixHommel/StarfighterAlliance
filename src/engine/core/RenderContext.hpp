4e6f236dded6b73722e60f580fb51f16a6304077
FelixHommel <felixhommel04@gmail.com>
2026-03-05 13:32:13 +0100

    feat: add RenderContext placeholder
    Needs implementation

---
+++ src/engine/core/RenderContext.hpp
@@ -0,0 +1,15 @@
+#ifndef SFA_SRC_ENGINE_CORE_RENDER_CONTEXT_HPP
+#define SFA_SRC_ENGINE_CORE_RENDER_CONTEXT_HPP
+
+namespace sfa
+{
+
+struct RenderContext
+{
+    // FIXME: add the members to the RenderContext
+};
+
+} // namespace sfa
+
+#endif // !SFA_SRC_ENGINE_CORE_RENDER_CONTEXT_HPP
+
