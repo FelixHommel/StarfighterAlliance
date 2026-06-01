a8fe50421feef0ecb4e5d47e35dd472301bff9ae
FelixHommel <felixhommel04@gmail.com>
2026-06-01 13:18:14 +0200

    feat: add enum convenience features

--- src/engine/config/SpaceshipColor.hpp
+++ src/engine/config/SpaceshipColor.hpp
@@ -2,6 +2,8 @@
 #define SFA_SRC_ENGINE_CONFIG_SPACESHIP_COLOR_HPP
 
 #include <cstdint>
+#include <format>
+#include <string>
 
 namespace sfa
 {
@@ -23,6 +25,48 @@ enum class SpaceshipColor : std::uint8_t
     Transparent
 };
 
+namespace spaceshipColor
+{
+
+[[nodiscard]] constexpr std::string toString(SpaceshipColor color) noexcept
+{
+    switch(color)
+    {
+        using enum SpaceshipColor;
+    case Red:
+        return "red";
+    case Blue:
+        return "blue";
+    case Yellow:
+        return "yellow";
+    case Orange:
+        return "ornage";
+    case Green:
+        return "green";
+    case Violet:
+        return "violet";
+    case White:
+        return "white";
+    case Black:
+        return "black";
+    case Transparent:
+        return "transparent";
+    }
+}
+
+} // namespace spaceshipColor
+
 } // namespace sfa
+
+template<>
+struct std::formatter<sfa::SpaceshipColor> : public std::formatter<std::string>
+{
+    using Parent = std::formatter<std::string>;
+
+    auto format(sfa::SpaceshipColor color, std::format_context& ctx) const
+    {
+        return Parent::format(sfa::spaceshipColor::toString(color), ctx);
+    }
+};
 
 #endif // !SFA_SRC_ENGINE_CONFIG_SPACESHIP_COLOR_HPP
--- src/engine/config/SpaceshipType.hpp
+++ src/engine/config/SpaceshipType.hpp
@@ -2,6 +2,8 @@
 #define SFA_SRC_ENGINE_CONFIG_SPACESHIP_TYPE_HPP
 
 #include <cstdint>
+#include <format>
+#include <string>
 
 namespace sfa
 {
@@ -18,6 +20,36 @@ enum class SpaceshipType : std::uint8_t
     SPACESHIP_TPYE_MAX = 3
 };
 
+namespace spaceshipType
+{
+
+[[nodiscard]] constexpr std::string toString(SpaceshipType type) noexcept
+{
+    switch(type)
+    {
+        using enum SpaceshipType;
+    case XWing:
+        return "x_wing";
+    case YWing:
+        return "y_wing";
+    case MilleniumFalcon:
+        return "millenium_falcon";
+    default:
+        return "unknown";
+    }
+}
+
+} // namespace spaceshipType
+
 } // namespace sfa
+
+template<>
+struct std::formatter<sfa::SpaceshipType> : public std::formatter<std::string>
+{
+    auto format(sfa::SpaceshipType type, std::format_context& ctx) const
+    {
+        return std::formatter<std::string>::format(sfa::spaceshipType::toString(type), ctx);
+    }
+};
 
 #endif // !SFA_SRC_ENGINE_CONFIG_SPACESHIP_TYPE_HPP
